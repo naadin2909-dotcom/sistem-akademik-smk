2971b917906fa10f6b3d450426e5cbc6deb06921
dina septiana <naadin2909@gmail.com>
2026-07-26 13:07:09 +0700

    Fix: use php -S directly instead of artisan serve for Railway

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 php artisan config:clear
 PORT_INT=$(php -r "echo (int)getenv('PORT');")
-php artisan serve --host=0.0.0.0 --port=$PORT_INT
+php -S 0.0.0.0:$PORT_INT -t public/ public/router.php
